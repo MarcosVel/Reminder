f33c14d3279d5c6c6dc46b8cc7772f2a6599ecb8
MarcosVel <marcosvelosodev@gmail.com>
2025-01-08 21:12:43 -0300

    feat[WIP]: showing bottom sheet on click

--- Reminder/Sources/Features/Splash/SplashViewController.swift
+++ Reminder/Sources/Features/Splash/SplashViewController.swift
@@ -13,6 +13,7 @@ class SplashViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupGesture()
         
         setup()
     }
@@ -33,5 +34,20 @@ class SplashViewController: UIViewController {
         ])
         
         contentView.translatesAutoresizingMaskIntoConstraints = false // required for UIKit
+    }
+    
+    private func setupGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(showLoginBottomSheet))
+        self.view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc
+    private func showLoginBottomSheet() {
+        let loginBottomSheet = LoginBSViewController()
+        loginBottomSheet.modalPresentationStyle = .overCurrentContext
+        loginBottomSheet.modalTransitionStyle = .crossDissolve
+        self.present(loginBottomSheet, animated: false) {
+            loginBottomSheet.animateShow()
+        }
     }
 }
