6f8a2902beb1df39636a3729f23833bfd17ce97b
MarcosVel <marcosvelosodev@gmail.com>
2025-01-17 14:54:33 -0300

    feat: firebase auth

--- Reminder/Sources/Features/LoginBottomSheet/ViewModel/LoginBSViewModel.swift
+++ Reminder/Sources/Features/LoginBottomSheet/ViewModel/LoginBSViewModel.swift
@@ -6,9 +6,17 @@
 //
 
 import Foundation
+import Firebase
 
 class LoginBSViewModel {
     func doAuth(userEmail: String, password: String) {
-        print("userEmail", userEmail)
+        Auth.auth().signIn(withEmail: userEmail, password: password) { [weak self] authResult, error in
+            if let error = error {
+                print("Auth Error: \(error)")
+                return
+            } else {
+                print(authResult, "Auth Success")
+            }
+        }
     }
 }
