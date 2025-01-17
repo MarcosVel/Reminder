2cce61b1f9e9f7e85c7a241bf978ef05d239a844
MarcosVel <marcosvelosodev@gmail.com>
2025-01-17 02:33:21 -0300

    feat: view model getting data from input

---
+++ Reminder/Sources/Features/LoginBottomSheet/ViewModel/LoginBSViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  LoginBSViewModel.swift
+//  Reminder
+//
+//  Created by Marcos Roberto Lemos Veloso on 17/01/25.
+//
+
+import Foundation
+
+class LoginBSViewModel {
+    func doAuth(userEmail: String, password: String) {
+        print("userEmail", userEmail)
+    }
+}
