8419b395bea72c77a9323d2ca0e891624b8def91
MarcosVel <marcosvelosodev@gmail.com>
2025-02-12 16:06:20 -0300

    feat: creating userDefaults

---
+++ Reminder/Sources/Features/Models/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Reminder
+//
+//  Created by Marcos Roberto Lemos Veloso on 12/02/25.
+//
+
+import Foundation
+
+struct User: Codable {
+    let email: String
+    let isUserSaved: Bool
+}
